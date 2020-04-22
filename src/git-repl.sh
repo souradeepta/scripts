bbbf3a75059d0280b8765749be9d2af2b9d52135
souradeepta <sdb.svnit@gmail.com>
2020-04-21 23:04:39 -0500

    git repl script

---
+++ src/git-repl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -u
+# make sure you make the file executable chmod +x
+
+
+if ! [ -n "${REPL_USING_RLWRAP:-}" ]; then
+  # Use `rlwrap` if available.
+  if which rlwrap > /dev/null 2>&1; then
+    exec env REPL_USING_RLWRAP=1 rlwrap "$0" "$@"
+  else
+    echo 'Install `rlwrap` and re-run this script to get command history.'
+  fi
+fi
+
+echo "This is the Git REPL; exit via Ctrl-D."
+
+while true; do
+  echo -n "$(pwd)> git "
+  read command || break
+
+  git $command
+done
+
+# Add a blank line at the end for a clean prompt when exiting.
+echo
